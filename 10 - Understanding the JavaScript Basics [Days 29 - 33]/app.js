03e5c32b52583193f52eefd911cb4a8467d89447
Ahmed Gamal <ahmgy.mail@gmail.com>
2024-04-21 02:52:10 +0200

    Finish days 30-31 learn basics of javascript

---
+++ 10 - Understanding the JavaScript Basics [Days 29 - 33]/app.js
@@ -0,0 +1,45 @@
+/****************/
+/* INTRODUCTORY */
+/****************/
+
+// STRINGS - NUMBERS - VARIABLES
+// let age = 24;
+// let greetingText = "Hi, I'm Ahmed!";
+// alert(greetingText);
+// greetingText = "Hi, I'm Ahmed Gamal!";
+// alert(greetingText);
+// alert(age);
+// age = 28;
+// alert(age);
+
+// ARRAYS
+let age = 32;
+let userName = "Max";
+let hobbies = ["Sports", "Cooking", "Reading"];
+// alert(hobbies);
+// alert(hobbies[0]);
+
+// OBJECTS
+let job = {
+  title: "Developer",
+  place: "New York",
+  salary: 50000,
+};
+// alert(job.title);
+
+// OPERATIONS
+let adult = age - 18;
+let bonus = job.salary + 3000;
+// alert(adult);
+// alert(bonus);
+
+// FUNCTIONS
+let totalAdultYears;
+function calculateAdultYears(userAge) {
+  return userAge - 18;
+}
+totalAdultYears = calculateAdultYears(age);
+alert(totalAdultYears);
+age = 45;
+totalAdultYears = calculateAdultYears(age);
+alert(totalAdultYears);
