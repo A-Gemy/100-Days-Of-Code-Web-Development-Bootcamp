b1692f98262d067bb0cd822e524af079015ffa92
Ahmed Gamal <ahmgy.mail@gmail.com>
2024-04-27 04:14:17 +0300

    Finish day 34 working with dom and solve an exercise

--- 11 - Advanced JavaScript Working with the DOM [Days 33 - 37]/app.js
+++ 11 - Advanced JavaScript Working with the DOM [Days 33 - 37]/app.js
@@ -5,4 +5,10 @@
 // console.dir(document);
 
 // EXAMPLE
-document.body.children[1].children[0].href = "https://google.com";
+// document.body.children[1].children[0].href = "https://google.com";
+// Another way - (It's much more convenient)
+let anchorElement = document.getElementById("external-link");
+anchorElement.href = "https://google.com";
+// Another way
+anchorElement = document.querySelector("p a"); // p a { color: red; }
+anchorElement.href = "https://youtube.com";
