a5586b727f50458322619e2127155db44600d291
Ahmed Gamal <ahmgy.mail@gmail.com>
2024-05-16 04:17:16 +0300

    Finish day 42 - handle form submission and submitted data

--- 13 - Milestone Project Building a Tic-Tac-Toe Game [Days 40 - 44]/js/config.js
+++ 13 - Milestone Project Building a Tic-Tac-Toe Game [Days 40 - 44]/js/config.js
@@ -1,4 +1,5 @@
-function openPlayerConfig() {
+function openPlayerConfig(e) {
+  editedPlayer = +e.target.dataset.playerid;
   playerConfigOverlayElement.style.display = "block";
   backdropElement.style.display = "block";
 }
@@ -6,4 +7,28 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = "none";
   backdropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = "";
+  inputElement.value = "";
+}
+
+function savePlayerConfig(e) {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const enteredPlayerName = formData.get("player-name").trim();
+
+  if (!enteredPlayerName) {
+    e.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "Please enter a valid name!";
+    return;
+  }
+
+  const updatedPlayerDataElement = document.getElementById(
+    "player-" + editedPlayer + "-data"
+  );
+  updatedPlayerDataElement.children[1].textContent = enteredPlayerName;
+
+  players[editedPlayer - 1].name = enteredPlayerName;
+
+  closePlayerConfig();
 }
