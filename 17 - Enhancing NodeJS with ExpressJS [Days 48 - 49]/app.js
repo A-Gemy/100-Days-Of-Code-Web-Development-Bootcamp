21450c58223abd7cbfc0ba3c13997463e28c9316
Ahmed Gamal <ahmgy.mail@gmail.com>
2024-05-30 03:06:14 +0300

    Finish module - return dynamic responses and work with nodemon

--- 17 - Enhancing NodeJS with ExpressJS [Days 48 - 49]/app.js
+++ 17 - Enhancing NodeJS with ExpressJS [Days 48 - 49]/app.js
@@ -17,7 +17,7 @@ app.get("/currentyear", function (req, res) {
 
 app.get("/", function (req, res) {
   res.send(
-    `<form action="/store-user" method="POST"><label>Your Name</label> <input type="text" name="username"> <button>Submit</button></form>`
+    `<form action="/store-user" method="POST"><label>Your Name: </label> <input type="text" name="username"> <button>Submit</button></form>`
   );
 });
 
@@ -34,6 +34,20 @@ app.post("/store-user", function (req, res) {
   fs.writeFileSync(filePath, JSON.stringify(existingUsers));
 
   res.send("<h1>Username stored!</h1>");
+});
+
+app.get("/users", function (req, res) {
+  const filePath = path.join("data", "users.json");
+  const fileData = fs.readFileSync(filePath);
+  const existingUsers = JSON.parse(fileData);
+
+  let responseData = "<ul>";
+  for (const user of existingUsers) {
+    responseData += "<li>" + user + "</li>";
+  }
+  responseData += "</ul";
+
+  res.send(responseData);
 });
 
 app.listen(3000);
