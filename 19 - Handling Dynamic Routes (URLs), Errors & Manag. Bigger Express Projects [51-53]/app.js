4a19526547b3f2c1b43fd4f0c6565c53449ead55
Ahmed Gamal <ahmgy.mail@gmail.com>
2024-06-09 04:13:35 +0300

    Finish day 51 - work with dynamic routes and use unique ids with uuid package

--- 19 - Handling Dynamic Routes (URLs), Errors & Manag. Bigger Express Projects [51-53]/app.js
+++ 19 - Handling Dynamic Routes (URLs), Errors & Manag. Bigger Express Projects [51-53]/app.js
@@ -1,39 +1,47 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
+const uuid = require("uuid");
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', function (req, res) {
-  res.render('index');
+app.get("/", function (req, res) {
+  res.render("index");
 });
 
-app.get('/restaurants', function (req, res) {
-  const filePath = path.join(__dirname, 'data', 'restaurants.json');
+app.get("/restaurants", function (req, res) {
+  const filePath = path.join(__dirname, "data", "restaurants.json");
 
   const fileData = fs.readFileSync(filePath);
   const storedRestaurants = JSON.parse(fileData);
 
-  res.render('restaurants', {
+  res.render("restaurants", {
     numberOfRestaurants: storedRestaurants.length,
     restaurants: storedRestaurants,
   });
 });
 
-app.get('/recommend', function (req, res) {
-  res.render('recommend');
+// Dynamic Routes
+app.get("/restaurants/:id", function (req, res) {
+  const restaurantId = req.params.id;
+  res.render("restaurant-detail", { rid: restaurantId });
 });
 
-app.post('/recommend', function (req, res) {
+app.get("/recommend", function (req, res) {
+  res.render("recommend");
+});
+
+app.post("/recommend", function (req, res) {
   const restaurant = req.body;
-  const filePath = path.join(__dirname, 'data', 'restaurants.json');
+  restaurant.id = uuid.v4();
+  const filePath = path.join(__dirname, "data", "restaurants.json");
 
   const fileData = fs.readFileSync(filePath);
   const storedRestaurants = JSON.parse(fileData);
@@ -42,15 +50,15 @@ app.post('/recommend', function (req, res) {
 
   fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
 
-  res.redirect('/confirm');
+  res.redirect("/confirm");
 });
 
-app.get('/confirm', function (req, res) {
-  res.render('confirm');
+app.get("/confirm", function (req, res) {
+  res.render("confirm");
 });
 
-app.get('/about', function (req, res) {
-  res.render('about');
+app.get("/about", function (req, res) {
+  res.render("about");
 });
 
 app.listen(3000);
