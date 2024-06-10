8b0ae494a45c9da865f78da2c61b07042ea83f8d
Ahmed Gamal <ahmgy.mail@gmail.com>
2024-06-10 04:20:16 +0300

    Finish day 52 - start refactoring files

--- 19 - Handling Dynamic Routes (URLs), Errors & Manag. Bigger Express Projects [51-53]/app.js
+++ 19 - Handling Dynamic Routes (URLs), Errors & Manag. Bigger Express Projects [51-53]/app.js
@@ -17,11 +17,6 @@ app.get("/", function (req, res) {
 });
 
 app.get("/restaurants", function (req, res) {
-  const filePath = path.join(__dirname, "data", "restaurants.json");
-
-  const fileData = fs.readFileSync(filePath);
-  const storedRestaurants = JSON.parse(fileData);
-
   res.render("restaurants", {
     numberOfRestaurants: storedRestaurants.length,
     restaurants: storedRestaurants,
@@ -42,7 +37,7 @@ app.get("/restaurants/:id", function (req, res) {
     }
   }
 
-  res.render("404");
+  res.status(404).render("404");
 });
 
 app.get("/recommend", function (req, res) {
@@ -52,14 +47,12 @@ app.get("/recommend", function (req, res) {
 app.post("/recommend", function (req, res) {
   const restaurant = req.body;
   restaurant.id = uuid.v4();
-  const filePath = path.join(__dirname, "data", "restaurants.json");
 
-  const fileData = fs.readFileSync(filePath);
-  const storedRestaurants = JSON.parse(fileData);
+  const restaurants = getStoredRestaurants();
 
-  storedRestaurants.push(restaurant);
+  restaurants.push(restaurant);
 
-  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+  storeRestaurants(restaurants);
 
   res.redirect("/confirm");
 });
@@ -73,11 +66,11 @@ app.get("/about", function (req, res) {
 });
 
 app.use(function (req, res) {
-  res.render("404");
+  res.status(404).render("404");
 });
 
 app.use(function (error, req, res, next) {
-  res.render("500");
+  res.status(500).render("500");
 });
 
 app.listen(3000);
