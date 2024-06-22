8d8467e61e4fa9e2e89b6bd78c4d888945f84e17
Ahmed Gamal <ahmgy.mail@gmail.com>
2024-06-22 04:19:28 +0300

    Learn about variable scoping & shadowing

--- 20 - Exploring More Advanced JavaScript Concepts [Days 54 - 56]/errors.js
+++ 20 - Exploring More Advanced JavaScript Concepts [Days 54 - 56]/errors.js
@@ -1,15 +1,38 @@
 /******************/
 /* ERROR HANDLING WITH TRY CATCH */
 /******************/
+// const fs = require("fs");
+
+// function readFile() {
+//   try {
+//     const fileData = fs.readFileSync("data.json");
+//   } catch (error) {
+//     console.log(error.message);
+//     console.log("An error occurred!");
+//   }
+//   console.log("Hi there!");
+// }
+
+// readFile();
+
+/******************/
+/* VARIABLE SCOPING & SHADOWING */
+/******************/
 const fs = require("fs");
 
 function readFile() {
+  let fileData;
   try {
-    const fileData = fs.readFileSync("data.json");
+    /* If I use const to define fileData in this block will be fine
+    since it will not related to outer one in spite of they 
+    have the same name (Shadowing) */
+    fileData = fs.readFileSync("data.json");
   } catch (error) {
     console.log(error.message);
     console.log("An error occurred!");
   }
+  // This will throw an error if variable was defined in try block (Block Scoping)
+  // console.log(fileData);
   console.log("Hi there!");
 }
 
