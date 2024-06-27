b248aaf29e510061d5b28d1e2942ffe8acf1267d
Ahmed Gamal <ahmgy.mail@gmail.com>
2024-06-27 04:23:24 +0300

    Finish module with async await

--- 20 - Exploring More Advanced JavaScript Concepts [Days 54 - 56]/async.js
+++ 20 - Exploring More Advanced JavaScript Concepts [Days 54 - 56]/async.js
@@ -21,15 +21,55 @@
 /* PROMISES */
 /******************/
 
-const { error } = require("console");
+// const fs = require("fs/promises");
+
+// function readFile() {
+//   let fileData;
+
+//   // This is (readFile) a asynchronous method differ from (readFileSync) which the code will not wait until it finish its execution
+//   // fileData = fs.readFile("data.txt", function (error, fileData) {
+//   /* IF ERRORS HAPPENED WITH ASYNCHRONOUS WE COULD DEAL WITH IT LIKE THIS */
+//   // if (error) {
+//   // ...
+//   // }
+//   //   console.log("File parsing done!");
+//   //   console.log(fileData.toString());
+//   //   // Start another async task that sends the data to a database
+//   // });
+
+//   /* IF ERRORS HAPPENED WITH PROMISES WE USE THE CATCH METHOD */
+//   fs.readFile("data.txt")
+//     .then(function (fileData) {
+//       console.log("File parsing done!");
+//       console.log(fileData.toString());
+//       const valueForAsyncOperation = 10;
+//       return valueForAsyncOperation;
+//     })
+//     .then(function (valueForAsyncOperation) {
+//       console.log(
+//         `The value returned from previous function is: ${valueForAsyncOperation}`
+//       );
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//     });
+
+//   // This will appear first
+//   console.log("Hi there!");
+// }
+
+// readFile();
+
+/******************/
+/* IMPROVING CODE WITH ASYNC AWAIT */
+/******************/
+
 const fs = require("fs/promises");
 
-function readFile() {
+async function readFile() {
   let fileData;
 
-  // This is (readFile) a asynchronous method differ from (readFileSync) which the code will not wait until it finish its execution
-  // fileData = fs.readFile("data.txt", function (error, fileData) {
-  /* IF ERRORS HAPPENED WITH ASYNCHRONOUS WE COULD DEAL WITH IT LIKE THIS */
+  // fs.readFile("data.txt", function (error, fileData) {
   // if (error) {
   // ...
   // }
@@ -38,24 +78,15 @@ function readFile() {
   //   // Start another async task that sends the data to a database
   // });
 
-  /* IF ERRORS HAPPENED WITH PROMISES WE USE THE CATCH METHOD */
-  fs.readFile("data.txt")
-    .then(function (fileData) {
-      console.log("File parsing done!");
-      console.log(fileData.toString());
-      const valueForAsyncOperation = 10;
-      return valueForAsyncOperation;
-    })
-    .then(function (valueForAsyncOperation) {
-      console.log(
-        `The value returned from previous function is: ${valueForAsyncOperation}`
-      );
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  try {
+    fileData = await fs.readFile("data.txt");
+  } catch (error) {
+    console.log(error);
+  }
 
-  // This will appear first
+  console.log("File parsing done!");
+  console.log(fileData.toString());
+  // return valueForAsyncOperation;
   console.log("Hi there!");
 }
 
